b9ed64be47b22fa48338b7811ddc957048c89db6
Dan Vallejo <dan.vallejo@nordstrom.com>
2017-03-28 20:08:52 -0700

    add snackbar

--- Android310/app/src/main/java/com/cstructor/android310/MainActivity.java
+++ Android310/app/src/main/java/com/cstructor/android310/MainActivity.java
@@ -1,7 +1,13 @@
 package com.cstructor.android310;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.uxSnackbar)
+    public void onSnackbar(View view){
+        Snackbar
+                .make(view, "I want a carrot", Snackbar.LENGTH_LONG)
+                .setAction("I want cake", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getApplicationContext(), "Here's your cake", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .show();
     }
 }
